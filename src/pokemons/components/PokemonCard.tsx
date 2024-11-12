5ac3992e2f01c7b320190db9c9cbad7912853abf
RSLastra <raul.slastra@gmail.com>
2024-11-12 09:14:30 -0600

    feat: pokemon page

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -17,7 +17,6 @@ export const PokemonCard = ({ id, name }: SimplePokemon) => {
             priority={false}
           />
           <p className="pt-2 text-lg font-semibold text-gray-50">{name}</p>
-          <p className="text-sm text-gray-100">John@Doe.com</p>
           <div className="mt-5">
             <Link href={`/dashboard/pokemon/${id}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
