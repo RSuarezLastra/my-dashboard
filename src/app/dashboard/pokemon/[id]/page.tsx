f46cda66bb5050d5867ed12776037626485e7443
RSLastra <raul.slastra@gmail.com>
2024-11-11 19:54:46 -0600

    feat: fetch pokemon data

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,24 @@
+import { Pokemon } from "@/pokemons";
 
 interface Props {
-  params: {id: string}
+  params: { id: string }
 }
 
-export default function PokemonPage({params}: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(res => res.json());
+  
+  return pokemon;
+}
+
+export default async function PokemonPage({ params }: Props) {
+
+  const pokemon = await getPokemon(params.id)
+
   return (
     <div>
-      <h1>Pokemon {params.id}</h1>
+      <h1>{JSON.stringify(pokemon)}</h1>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,6 +1,7 @@
 //interfaces
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
 
 //components
 export { PokemonGrid } from './components/PokemonGrid';
