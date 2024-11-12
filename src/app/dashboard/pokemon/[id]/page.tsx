f2c61734167d0c42e47e25d5fc599973a83ad088
RSLastra <raul.slastra@gmail.com>
2024-11-11 20:18:46 -0600

    feat: dynamic metadata

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,14 +1,27 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string }
 }
 
+export async function generateMetada({ params }: Props): Promise<Metadata> {
+
+  const { id, name } = await getPokemon(params.id)
+
+  return {
+    title: `#${id} - ${name}`,
+    description: `Página del pokemon ${name}`
+  }
+}
+
 const getPokemon = async (id: string): Promise<Pokemon> => {
-  
-  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache'
+  })
     .then(res => res.json());
-  
+
   return pokemon;
 }
 
@@ -18,7 +31,7 @@ export default async function PokemonPage({ params }: Props) {
 
   return (
     <div>
-      <h1>{JSON.stringify(pokemon)}</h1>
+      <h1>{pokemon.name}</h1>
     </div>
   );
 }
